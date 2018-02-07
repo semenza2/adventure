15a3397b7a645a8ccfeca1d8a7770cba4fde3446
Sarah Menza <semenza2@illinois.edu>
2018-02-06 23:10:30 -0600

    Game works, woohoo!!

--- src/com/example/Adventure.java
+++ src/com/example/Adventure.java
@@ -1,15 +1,17 @@
 package com.example;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import org.apache.commons.codec.binary.StringUtils;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.net.MalformedURLException;
 import java.util.Scanner;
 
 public class Adventure {
 
-    private static Room currentRoom;
+    private static Room currentRoom = new Room();
+    private static ArrayList<String> currentItems;
+    private static ArrayList<String> myItems;
     private static Layout layout;
-    private static Room[] rooms = layout.getRooms();
+    private static Room[] rooms;
     private static String originalInput;
     private static boolean gameIsOver=false;
 
@@ -21,17 +23,18 @@ public class Adventure {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        start(layout);
     }
 
     public static void start(Layout l) {
         layout = l;
-        Room[] rooms = layout.getRooms();
+        myItems = new ArrayList<>();
+        rooms = layout.getRooms();
         for (int i = 0; i < rooms.length; i++) {
-            if (rooms[i].getName() == layout.getStartingRoom()) {
+            if (rooms[i].getName().equals(layout.getStartingRoom())) {
                 currentRoom = rooms[i];
             }
         }
+        itemsToArray();
         statusReport();
     }
 
@@ -44,20 +47,21 @@ public class Adventure {
             System.out.println("Your journey begins here");
         } else if (currentRoom.getName().equals(layout.getEndingRoom())){
             System.out.println("You have reached your final destination");
+            gameIsOver = true;
         }
 
-        if(currentRoom.getItems().length == 0) {
+        if(currentItems.size() == 0) {
             System.out.println("This room contains nothing");
         } else {
             System.out.println("The current items in your room are: ");
-            for (String e : currentRoom.getItems()) {
+            for (String e : currentItems) {
                 System.out.println(e);
             }
         }
 
         System.out.println("The directions you can move are: ");
         for (Direction e: currentRoom.getDirections()) {
-            System.out.println(e);
+            System.out.println(e.getDirectionName());
         }
     }
 
@@ -69,12 +73,11 @@ public class Adventure {
         if (input.equals("exit") || input.equals("quit")) {
             gameIsOver = true;
             System.exit(0);
-        }
-        if (input.contains("go")) {
+        } else if (input.contains("go")) {
             boolean invalidInput = true;
             for (int i = 0; i < currentRoom.getDirections().length; i++){
                 //making sure input is an option
-                if (input.contains(currentRoom.getDirections()[i].getDirectionName())) {
+                if (input.contains(currentRoom.getDirections()[i].getDirectionName().toLowerCase())) {
                     invalidInput = false;
                     System.out.println(originalInput);
                     move(currentRoom.getDirections()[i].getRoom());
@@ -84,6 +87,58 @@ public class Adventure {
             if(invalidInput) {
                 System.out.println("I can't go that direction");
             }
+        } else if (input.contains("take")) {
+            boolean invalidInput = true;
+            for (int i = 0; i < currentItems.size(); i++){
+                //making sure input is an option
+                if (input.contains(currentItems.get(i))) {
+                    invalidInput = false;
+                    System.out.println(originalInput);
+                    take(currentItems.get(i));
+                    break;
+                }
+            }
+            if(invalidInput) {
+                System.out.println("I can't take that item");
+            }
+        } else if (input.contains("drop")) {
+            boolean invalidInput = true;
+            for (int i = 0; i < myItems.size(); i++) {
+                //making sure input is an option
+                if (input.contains(myItems.get(i))) {
+                    invalidInput = false;
+                    System.out.println(originalInput);
+                    drop(myItems.get(i));
+                    break;
+                }
+            }
+            if (invalidInput) {
+                System.out.println("I can't drop that item");
+            }
+        }
+        statusReport();
+    }
+
+    public static void drop(String item) {
+        myItems.remove(item);
+        currentItems.add(item);
+    }
+
+    public static void itemsToArray() {
+        if (currentRoom.getItems() != null) {
+            currentItems = new ArrayList<>(Arrays.asList(currentRoom.getItems()));
+        }else {
+            currentItems = new ArrayList<>();
+        }
+    }
+
+    public static void take(String item) {
+        myItems.add(item);
+        currentItems.remove(item);
+        for (int i = 0;  i <currentItems.size(); i++) {
+            if(currentItems.get(i).equals(item)) {
+                currentItems.remove(i);
+            }
         }
     }
 
@@ -91,7 +146,7 @@ public class Adventure {
        for(int i = 0; i < rooms.length; i++) {
            if (rooms[i].getName().equals(room)) {
                currentRoom = rooms[i];
-               statusReport();
+               itemsToArray();
                break;
            }
        }
